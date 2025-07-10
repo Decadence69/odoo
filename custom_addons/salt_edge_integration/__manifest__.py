ff36319414d97bcc426feb811f89251320a60488
Yeo Szy Qi <szyqiyeo1010@gmail.com>
2025-07-10 14:49:55 +0800

    Update __manifest__.py

--- custom_addons/salt_edge_integration/__manifest__.py
+++ custom_addons/salt_edge_integration/__manifest__.py
@@ -8,6 +8,7 @@
         'views/salt_edge_views.xml',
         'views/sync_wizard_view.xml',
         'data/payment_provider.xml',
+        'data/payment_acquirer.xml',
     ],
     'installable': True,
     'application': True,
